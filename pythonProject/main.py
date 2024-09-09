d4cedbe0d6714e89971f83dcd51ef4bd22c44e99
kfest <yunusova.k.b@gmail.com>
2024-09-09 22:42:55 +0500

    bubble 2.0

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,15 +1,24 @@
 from random import randint
+def bubble (a,k ):
+    if k==0 :
+        for i in range(N - 1):
+            for j in range(N - 1 - i):
+                if a[j] > a[j + 1]:
+                    a[j], a[j + 1] = a[j + 1], a[j]
+        return a
+    if k==1:
+        for i in range(N -1):
+            for j in range( N-1-i):
+                if a[j]<a[j+1]:
+                    a[j], a[j + 1] = a[j + 1], a[j]
+        return a
 
 N = int(input("Введите массив числа:"))
 a = []
 for i in range(N):
     a.append(randint(1, 99))
+
 print(a)  # вывод исходного неотсортированного списка
 
-# Сама сортировка методом "пузырька"
-for i in range(N-1):
-    for j in range(N-1-i):
-        if a[j] > a[j+1]:
-            a[j], a[j+1] = a[j+1], a[j]
-
-print(a)  # вывод отсортированного списка
+k = int(input("Как сортируем? \n 0 -  по возрастанию \n 1 - по убыванию \n"))
+print(bubble(a, k))  # вывод отсортированного списка
